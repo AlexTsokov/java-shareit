eae3c8905677b4105a117f84e444931f3b04abb4
AlexTsokov <tdkao@yandex.ru>
2023-06-07 22:50:21 +0300

    ТЗ16-3 style2

--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import ru.practicum.shareit.exception.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 
